c7e14e7775451d5f9c7e8a243ad7f254e4fff54f
riensWinoto <rienslw@outlook.com>
2019-04-25 14:25:13 +0700

    better readability code naming

--- GCSUploader.py
+++ GCSUploader.py
@@ -2,67 +2,72 @@ import sys
 # import os #use this when using environ for automatic assign Google Application Credentials
 from google.cloud import storage
 
+
 class Uploader:
 
-    def getBucketName(self, bucketName):
-        self.bucketName = bucketName
-
-        if self.bucketName == "":
-            self.bucketName = input("Please enter your bucket name: ")
-            if self.bucketName == "":
+    def get_bucket_name(self, bucket_name):
+        self.bucket_name = bucket_name
+        if self.bucket_name == "":
+            self.bucket_name = input("Please enter your bucket name: ")
+            if self.bucket_name == "":
                 print("Next time fill your bucket name")
                 sys.exit()
             else:
-                bucketNameGCS = clientBucket(self)
-                return bucketNameGCS
-
+                bucket_gcs = Uploader.client_bucket(self)
+                return bucket_gcs
         else:
-            bucketNameGCS = clientBucket(self)
-            return bucketNameGCS
+            bucket_gcs = Uploader.client_bucket(self)
+            return bucket_gcs
 
-    def enterDirNewContent(self, dirNewContent):
-        self.dirNewContent = dirNewContent
-        if self.dirNewContent == "":
-            return self.dirNewContent
+    def client_bucket(self):
+        client = storage.Client()
+        return client.get_bucket(self.bucket_name)
+
+    def new_content_directory(self, new_content_dir):
+        self.new_content_dir = new_content_dir
+        if self.new_content_dir == "":
+            return self.new_content_dir
         else:
-            self.dirNewContent = self.dirNewContent + "/"
-            return self.dirNewContent
+            self.new_content_dir = self.new_content_dir + "/"
+            return self.new_content_dir
 
-    def enterNewContentName(self, newContentName):
-        self.newContentName = newContentName
-        if self.newContentName == "":
-            self.newContentName = str(input("Please enter your desired file name include extension: "))
-            if self.newContentName == "":
+    def new_content_name(self, content_new_name):
+        self.content_new_name = content_new_name
+        if self.content_new_name == "":
+            self.content_new_name = str(input("Please enter your desired file name include extension: "))
+            if self.content_new_name == "":
                 print("Next time fill your desired file name include extension")
                 sys.exit()
             else:
-                return self.newContentName
-
+                return self.content_new_name
         else:
-            return self.newContentName
+            return self.content_new_name
 
-    def enterLocalContentDir(self, localContentDir):
-        self.localContentDir = localContentDir
-        return self.localContentDir
-
-def clientBucket(self):
-    client = storage.Client()
-    return client.get_bucket(self.bucketName)
+    def local_content_directory(self, content_directory):
+        self.content_directory = content_directory
+        if self.content_directory == "":
+            self.content_directory = str(input("Please enter your content directory to upload: "))
+            if self.content_directory == "":
+                print("Next time fill your content directory to upload")
+            else:
+                return self.content_directory
+        else:
+            return self.content_directory
 
 # initial stage
 if __name__ == "__main__":
-    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "yourServiceAccount.json"
-    gUploader = Uploader()
-    bucketIS = gUploader.getBucketName(input("Enter your bucket name: "))
+    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "yourServiceAccount.json" # or "path/to/serviceAccount.json
+    gcsUploader = Uploader()
+    gcsBucket = gcsUploader.get_bucket_name(input("Enter your bucket name: "))
 
 # looping stage
     countNum = 0
     while countNum < 1:
-        dirContentGCS = gUploader.enterDirNewContent(str(input("Enter directory for new content: ")))
-        nameContentGCS = gUploader.enterNewContentName(str(input("Enter your desired file name include extension: ")))
-        localDir = gUploader.enterLocalContentDir(str(input("Enter your content directory to upload: ")))
-        dirImgOut = dirContentGCS + nameContentGCS
-        blobGCS = bucketIS.blob(dirImgOut)
+        newDirGCS = gcsUploader.new_content_directory(str(input("Enter directory for new content: ")))
+        newNameGCS = gcsUploader.new_content_name(str(input("Enter your desired file name include extension: ")))
+        localDir = gcsUploader.local_content_directory(str(input("Enter your content directory to upload: ")))
+        pathOnGCS = newDirGCS + newNameGCS
+        blobGCS = gcsBucket.blob(pathOnGCS)
         blobGCS.upload_from_filename(filename=localDir)
-        print("This is your content directory and name: " + dirImgOut)
+        print("This is your content directory and name: " + pathOnGCS)
         print("this is your content you upload to GCS: " + localDir + "\n")
